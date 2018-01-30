41f17e5962f6a342efebafee2a8c3f2823850ee9
zhubo <670403684@qq.com>
2018-01-30 23:48:40 +0800

    init

---
+++ src/main/java/io/netty/handler/codec/msgpack/EchoClientHandler.java
@@ -0,0 +1,11 @@
+package io.netty.handler.codec.msgpack;
+
+import io.netty.channel.ChannelHandlerAdapter;
+
+/**
+ * Created by zhubo on 2018/1/30.
+ */
+public class EchoClientHandler extends ChannelHandlerAdapter {
+
+
+}
